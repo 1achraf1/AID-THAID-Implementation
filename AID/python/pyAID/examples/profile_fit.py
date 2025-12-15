d510e3cc2ed1d455f3a5078aa4918c8fabf1018c
ZIDEA <djeryala@gmail.com>
2025-12-15 17:37:42 +0100

    Delete AID/python/pyAID directory

--- AID/python/pyAID/examples/profile_fit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from pyAID import AIDRegressor
-
-rng = np.random.default_rng(0)
-X = rng.normal(size=(50000, 10))
-y = 2*X[:,0] + 0.5*(X[:,1]>0) + rng.normal(size=50000)
-
-m = AIDRegressor(Q=6, M=40, R=20, min_gain=1e-3)
-m.fit(X, y)
